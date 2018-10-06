ad8de3b56bde0dc555d2693ae7f9d55afbefacdb
Jinliang Yang <yangjl0930@gmail.com>
2018-10-06 16:51:53 -0500

    tests

--- _rootsource/0.update_site.R
+++ _rootsource/0.update_site.R
@@ -3,7 +3,7 @@
 library("servr")
 jekyll(dir = ".", input = c(".", "_rootsource", "_postsource"),
        output = c(".",  ".", "_posts"), script = c("Makefile", "build.R"), serve = FALSE,
-       command = "bundle exec jekyll build")
+       command = "bundle exec jekyll serve")
 
 ### How to change markdown CSS elements
 ### https://gist.github.com/jonschlinkert/5854601
